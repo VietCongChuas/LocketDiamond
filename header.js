f4743023a4cc4ca217cb6941f83cd74b3fec1e5a
VietCongChuas <vtranquoc2802@gmail.com>
2026-06-22 12:13:37 +0700

    Update header.js

--- header.js
+++ header.js
@@ -1,5 +1,3 @@
-// deleteHeader.js - VietCongChuas
-// Updated deleteHeader.js
 const version = 'V1.0.3';
 
 function setHeaderValue(e, a, d) {
@@ -7,11 +5,14 @@ function setHeaderValue(e, a, d) {
   r in e ? e[r] = d : e[a] = d;
 }
 
+// Lấy headers hiện tại từ request
 var modifiedHeaders = $request.headers;
 
+// Thay đổi giá trị của X-RevenueCat-ETag
 setHeaderValue(modifiedHeaders, "X-RevenueCat-ETag", "");
 
+// Debug: In header đã sửa (tuỳ chọn)
 console.log("Modified Headers:", JSON.stringify(modifiedHeaders));
 
+// Kết thúc request với header đã sửa đổi
 $done({ headers: modifiedHeaders });
-// VietCongChuas
